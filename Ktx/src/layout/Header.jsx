727223535a5b5863555f5f2cd0db4da7982305a0
JoinWichIT <126174862+JoinWichIT@users.noreply.github.com>
2025-04-17 15:30:20 +0700

    e

---
+++ Ktx/src/layout/Header.jsx
@@ -0,0 +1,14 @@
+import { Winicon } from "wini-web-components";
+
+
+const Header =()=>{
+    return(
+        <div className="row">
+           <div className="row">
+           <Winicon src="fill/layout/menu" />
+           </div>
+
+        </div>
+    )
+}
+export default Header;
---
+++ Ktx/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Header from "../layout/Header.jsx";
+
+
+function Home() {
+    return (
+        <div><Header/></div>
+    )
+}
+
+export default Home;
